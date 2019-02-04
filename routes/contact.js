aa48e9604d2ba628ba90db1f7f692ebaa5fed89d
WickedDeveloper <gwyn@dgfprofessionalservices.com>
2019-02-04 07:15:10 -0500

    created contact model

---
+++ routes/contact.js
@@ -0,0 +1,27 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// create a reference to the DB schema
+let contact = require('../models/contact');
+
+// GET Contact List page - READ Operation
+router.get('/', (req, res, next) => {
+    contact.find((err, contactList) => {
+        if(err) {
+            return console.error(err);
+        } 
+        else {
+            console.log(contactList);
+
+            /*
+            res.render('contacts/index'm {
+                title: 'Contact List',
+                contactList: contactList
+            });
+            */
+        }
+    });
+}); 
+
+module.exports = router;
