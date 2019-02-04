4811e89d6bc783dd69e22bc4ecfd140dc7317e3c
WickedDeveloper <gwyn@dgfprofessionalservices.com>
2019-02-04 08:55:53 -0500

    displayed DB in view

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    "URI" : "mongodb://localhost/test"
+   //"URI" : "mongodb://localhost/test"
+   "URI" : "mongodb://gwyner:password123@ds058548.mlab.com:58548/comp308-w2019-lesson4b-dg"
 }
