b809b280cb1bbc99e6db79eb2f9af15df189ef46
WickedDeveloper <gwyn@dgfprofessionalservices.com>
2019-02-06 09:02:46 -0500

    Updated URI and posted on GitHub

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 module.exports = {
    
    // URI for localhost testing
-   "URI" : "mongodb://localhost/test"
+   //"URI" : "mongodb://localhost/test"
+   //"URI" : "mongodb://localhost/davidgwyn "
 
    // URI used for in class work
    //"URI" : "mongodb://gwyner:password123@ds058548.mlab.com:58548/comp308-w2019-lesson4b-dg"
 
    // URI for davidgwyn database
-   //"URI" : "mongodb://admin:admin123456@ds064278.mlab.com:64278/comp308-w2019-mongodb-dg"
+   "URI" : "mongodb://admin:admin123456@ds064278.mlab.com:64278/comp308-w2019-mongodb-dg"
 }
